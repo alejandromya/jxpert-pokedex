48904953552503dbfd5982a15d998ab4aef83f4a
unknown <upnalab@eim-por-71425.dyc.a.unavarra.es>
2025-05-24 12:11:16 +0200

    Test: añadidos test de urls

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import pokemonList from "./pokemonListMock.json";
 
 describe("App Component", () => {
-  test("debería verse el nombre del bulbasaur cuando se cargan los datos", async () => {
+  test.only("debería verse el nombre del bulbasaur cuando se cargan los datos", async () => {
     const mockFetch = vi.fn();
     globalThis.fetch = mockFetch;
 
@@ -12,7 +12,7 @@ describe("App Component", () => {
       json: async () => ({
         results: [
           {
-            url: "https://pokeapi.co/api/v2/pokemon/1/",
+            url: "pokemonUrl",
           },
         ],
       }),
@@ -26,6 +26,11 @@ describe("App Component", () => {
 
     const bulbasaur = await screen.findByText("bulbasaur");
     expect(bulbasaur).toBeInTheDocument();
+    expect(mockFetch).toHaveBeenNthCalledWith(
+      1,
+      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=151",
+    );
+    expect(mockFetch).toHaveBeenNthCalledWith(2, "pokemonUrl");
   });
 
   test("debería verse el nombre del snorlax cuando se cargan los datos", async () => {
@@ -50,5 +55,29 @@ describe("App Component", () => {
 
     const snorlax = await screen.findByText("snorlax");
     expect(snorlax).toBeInTheDocument();
+  });
+
+  test("debería verse la imagen de bulbasaur cuando se cargan los datos", async () => {
+    const mockFetch = vi.fn();
+    globalThis.fetch = mockFetch;
+
+    const secondFetch = mockFetch.mockResolvedValueOnce({
+      json: async () => ({
+        results: [
+          {
+            url: "https://pokeapi.co/api/v2/pokemon/1/",
+          },
+        ],
+      }),
+    });
+
+    secondFetch.mockResolvedValueOnce({
+      json: async () => pokemonList[0],
+    });
+
+    render(<App />);
+
+    const bulbasaurArtwork = await screen.findByAltText("bulbasaur artwork");
+    expect(bulbasaurArtwork).toBeInTheDocument();
   });
 });

49ff8cce2f876959c0dabc42663cdaa0e86e3d27
unknown <upnalab@eim-por-71425.dyc.a.unavarra.es>
2025-05-30 18:20:08 +0200

    Test: Filtrado por región actualizado

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,140 +2,102 @@ import { describe, expect, test, vi } from "vitest";
 import { App } from "../App";
 import { render, screen } from "@testing-library/react";
 import pokemonMockBulbasaur from "./__fixtures__/pokemonMockBulbasaur.json";
-import pokemonMockSnorlax from "./__fixtures__/pokemonMockSnorlax.json";
 import pokemonMockServine from "./__fixtures__/pokemonMockServine.json";
 
 import userEvent from "@testing-library/user-event";
 
-let call: number = 0;
+const getGenerateMockFetch = () => {
+  let callNumber: number = 0;
 
-const generarMockFetch = async () => {
-  call++;
-  const returnedValueUrl = {
-    json: async () => ({
-      results: [
-        {
-          url: "url",
-        },
-      ],
-    }),
+  return {
+    fetchMock: async () => {
+      callNumber++;
+      const returnedValueUrl = {
+        json: async () => ({
+          results: [
+            {
+              url: "url",
+            },
+          ],
+        }),
+      };
+
+      const returnedValueBulbasaur = {
+        json: async () => pokemonMockBulbasaur,
+      };
+
+      const returnedValueServine = {
+        json: async () => pokemonMockServine,
+      };
+
+      if (callNumber === 1 || callNumber === 3) {
+        return returnedValueUrl;
+      } else if (callNumber === 2) {
+        return returnedValueBulbasaur;
+      } else if (callNumber === 4) {
+        return returnedValueServine;
+      }
+    },
   };
-
-  const returnedValueBulbasaur = {
-    json: async () => pokemonMockBulbasaur,
-  };
-
-  const returnedValueServine = {
-    json: async () => pokemonMockServine,
-  };
-
-  if (call === 1 || call === 3) {
-    return returnedValueUrl;
-  } else if (call === 2) {
-    return returnedValueBulbasaur;
-  } else if (call === 4) {
-    return returnedValueServine;
-  }
 };
-
 describe("App Component", () => {
-  test("deberian asegurarse las llamadas a la API", async () => {
+  test("debería mostrarse todos los datus de Bulbasaur", async () => {
     const mockFetch = vi.fn();
     globalThis.fetch = mockFetch;
-
-    mockFetch.mockImplementation(generarMockFetch);
+    const migenerador = getGenerateMockFetch();
+    mockFetch.mockImplementation(migenerador.fetchMock);
 
     render(<App />);
 
-    const bulbasaur = await screen.findByText("bulbasaur");
-    expect(bulbasaur).toBeInTheDocument();
-  });
-});
+    const bulbasaurName = await screen.findByText("bulbasaur");
+    expect(bulbasaurName).toBeInTheDocument();
+    const bulbasaurHp = await screen.findByText("Hp");
+    expect(bulbasaurHp).toBeInTheDocument();
+    const bulbasaurAt = await screen.findByText("At");
+    expect(bulbasaurAt).toBeInTheDocument();
+    const bulbasaurDf = await screen.findByText("Df");
+    expect(bulbasaurDf).toBeInTheDocument();
+    const bulbasaurSpA = await screen.findByText("SpA");
+    expect(bulbasaurSpA).toBeInTheDocument();
+    const bulbasaurSpD = await screen.findByText("SpD");
+    expect(bulbasaurSpD).toBeInTheDocument();
+    const bulbasaurSpd = await screen.findByText("Spd");
+    expect(bulbasaurSpd).toBeInTheDocument();
 
-test("debería verse el nombre del snorlax cuando se cargan los datos", async () => {
-  const mockFetch = vi.fn();
-  globalThis.fetch = mockFetch;
-
-  const secondFetch = mockFetch.mockResolvedValueOnce({
-    json: async () => ({
-      results: [
-        {
-          url: "https://pokeapi.co/api/v2/pokemon/1/",
-        },
-      ],
-    }),
-  });
-
-  secondFetch.mockResolvedValueOnce({
-    json: async () => pokemonMockSnorlax,
+    const bulbasaurArtwork = await screen.findByAltText("bulbasaur artwork");
+    expect(bulbasaurArtwork).toBeInTheDocument();
   });
 
-  render(<App />);
+  test("cuando pulsamos la combobutton se abre el listado", async () => {
+    const mockFetch = vi.fn();
+    globalThis.fetch = mockFetch;
 
-  const snorlax = await screen.findByText("snorlax");
-  expect(snorlax).toBeInTheDocument();
-});
+    mockFetch.mockImplementation(getGenerateMockFetch().fetchMock);
+    render(<App />);
 
-test("debería mostrarse todos los datus de Bulbasaur", async () => {
-  const mockFetch = vi.fn();
-  globalThis.fetch = mockFetch;
+    const bulbasaurName = await screen.findByText("bulbasaur");
+    expect(bulbasaurName).toBeInTheDocument();
+    const combobox = await screen.findByRole("combobox", {
+      name: "Select reg",
+    });
 
-  const secondFetch = mockFetch.mockResolvedValueOnce({
-    json: async () => ({
-      results: [
-        {
-          url: "https://pokeapi.co/api/v2/pokemon/1/",
-        },
-      ],
-    }),
+    await userEvent.click(combobox);
+    const region = screen.getByRole("radio", {
+      name: "unova",
+    });
+    await userEvent.click(region);
+
+    const servine = await screen.findByText("servine");
+
+    expect(servine).toBeInTheDocument();
   });
 
-  secondFetch.mockResolvedValueOnce({
-    json: async () => pokemonMockBulbasaur,
+  test.only("cuando cargamos la pagina hay un skeleton", async () => {
+    const mockFetch = vi.fn();
+    globalThis.fetch = mockFetch;
+
+    mockFetch.mockImplementation(getGenerateMockFetch().fetchMock);
+
+    render(<App />);
   });
-
-  render(<App />);
-
-  const bulbasaurName = await screen.findByText("bulbasaur");
-  expect(bulbasaurName).toBeInTheDocument();
-  const bulbasaurHp = await screen.findByText("Hp");
-  expect(bulbasaurHp).toBeInTheDocument();
-  const bulbasaurAt = await screen.findByText("At");
-  expect(bulbasaurAt).toBeInTheDocument();
-  const bulbasaurDf = await screen.findByText("Df");
-  expect(bulbasaurDf).toBeInTheDocument();
-  const bulbasaurSpA = await screen.findByText("SpA");
-  expect(bulbasaurSpA).toBeInTheDocument();
-  const bulbasaurSpD = await screen.findByText("SpD");
-  expect(bulbasaurSpD).toBeInTheDocument();
-  const bulbasaurSpd = await screen.findByText("Spd");
-  expect(bulbasaurSpd).toBeInTheDocument();
-
-  const bulbasaurArtwork = await screen.findByAltText("bulbasaur artwork");
-  expect(bulbasaurArtwork).toBeInTheDocument();
-});
-
-test.only("cuando pulsamos la combobutton se abre el listado", async () => {
-  const mockFetch = vi.fn();
-  globalThis.fetch = mockFetch;
-
-  mockFetch.mockImplementation(generarMockFetch);
-
-  render(<App />);
-
-  const bulbasaur = await screen.findByText("bulbasaur");
-  expect(bulbasaur).toBeInTheDocument();
-  const combobox = await screen.findByRole("combobox", {
-    name: "Select reg",
-  });
-
-  await userEvent.click(combobox);
-  const region = screen.getByRole("radio", {
-    name: "unova",
-  });
-  await userEvent.click(region);
-
-  const servine = await screen.findByText("servine");
-
-  expect(servine).toBeInTheDocument();
 });

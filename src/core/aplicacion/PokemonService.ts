652b0be045ef507d70d6e494cd17a0b5467f87b6
alejandromayapa <alexx.maya@gmail.com>
2025-06-17 18:32:00 +0200

    quitar return fetch

--- src/core/aplicacion/PokemonService.ts
+++ src/core/aplicacion/PokemonService.ts
@@ -2,11 +2,7 @@ import { PokemonRepository } from "../dominio/PokemonRepository";
 
 export class PokemonService {
   constructor(private pokemonRepository: PokemonRepository) {}
-  getPokemon = async (urlOffset: number, urlLimit: number) => {
-    const pokeResponse = await this.pokemonRepository.listPokemon(
-      urlOffset,
-      urlLimit,
-    );
-    return pokeResponse;
+  getPokemon = (urlOffset: number, urlLimit: number) => {
+    return this.pokemonRepository.listPokemon(urlOffset, urlLimit);
   };
 }

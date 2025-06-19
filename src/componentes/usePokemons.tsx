1507e67aa7c72ffedba4ea6ae26b77b88181701c
alejandromya <alexxx.maya@gmail.com>
2025-06-19 12:24:32 +0200

    refactor

--- src/componentes/usePokemons.tsx
+++ src/componentes/usePokemons.tsx
@@ -43,7 +43,7 @@ export const usePokemons = () => {
    * Filters results based on input query term.
    */
 
-  const filterPokemon = () => {
+  const filterPokemon = (filteredPokemon:Pokemon[], searchingText:string) => {
     const filteredPoks = filteredPokemon.filter(
       (res) =>
         res.name.includes(searchingText.toLowerCase()) ||
@@ -55,29 +55,10 @@ export const usePokemons = () => {
   };
 
   useEffect(() => {
-    setAllPokemons(filterPokemon());
+    setAllPokemons(filterPokemon(filteredPokemon, searchingText));
 
     setIsFilteringByText(false);
   }, [filteredPokemon[0]?.id, searchingText]);
-  /**
-   * Sorts results based on selected sorting criteria.
-   */
-
-  /* const sortByStat = (pokemons: Pokemon[], statName?: Stats["stat_name"]) => {
-    if (statName === "default") {
-      const pokemonsOrdered = [...pokemons].sort((pokemon1, pokemon2) => {
-        return pokemon1.id - pokemon2.id;
-      });
-      return pokemonsOrdered;
-    }
-
-    const pokemonsOrdered = [...pokemons].sort((pokemon1, pokemon2) => {
-      const pokemon1Stats = pokemon1.stats[statName!];
-      const pokemon2Stats = pokemon2.stats[statName!];
-      return pokemon2Stats - pokemon1Stats;
-    });
-    return pokemonsOrdered;
-  }; */
 
   useEffect(() => {
     const pokemons: Pokemon[] = sortByStat(allPokemons, sortedBy);
---
+++ src/core/aplicacion/FilterService.ts
@@ -0,0 +1,12 @@
+import { Pokemon } from "../../types/types";
+
+  export    const filterPokemon = (filteredPokemon:Pokemon[], searchingText:string) => {
+    const filteredPoks = filteredPokemon.filter(
+      (res) =>
+        res.name.includes(searchingText.toLowerCase()) ||
+        !!res.types.find((type) =>
+          type.type_name.startsWith(searchingText.toLowerCase()),
+        ),
+    );
+    return filteredPoks;
+  };

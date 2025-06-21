e214869e6d685614725a45ea83f7070d1ef7ea08
alejandromya <alexxx.maya@gmail.com>
2025-06-21 09:10:31 +0200

    refactor

--- src/core/aplicacion/FilterService.ts
+++ src/core/aplicacion/FilterService.ts
@@ -1,13 +1,15 @@
 import { Pokemon } from "../dominio/Pokemon";
 
-
-  export    const filterPokemon = (filteredPokemon:Pokemon[], searchingText:string) => {
-    const filteredPoks = filteredPokemon.filter(
-      (res) =>
-        res.name.includes(searchingText.toLowerCase()) ||
-        !!res.types.find((type) =>
-          type.type_name.startsWith(searchingText.toLowerCase()),
-        ),
-    );
-    return filteredPoks;
-  };
+export const filterPokemon = (
+  filteredPokemon: Pokemon[],
+  searchingText: string,
+) => {
+  const filteredPoks = filteredPokemon.filter(
+    (res) =>
+      res.name.includes(searchingText.toLowerCase()) ||
+      !!res.types.find((type) =>
+        type.type_name.startsWith(searchingText.toLowerCase()),
+      ),
+  );
+  return filteredPoks;
+};
